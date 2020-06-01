8207675e93ea35090be29b5f646350d5b69931df
gaurav <singhgaurav9698@gmail.com>
2020-06-01 22:27:38 +0530

    Bit Magic

---
+++ Bit Magic/Left_Shift.java
@@ -0,0 +1,17 @@
+
+public class Left_Shift {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int x = 5;
+        System.out.println("left shift 1 of x  is    :  "+(x<<1));
+        System.out.println("left shift 2 of x  is    :  "+(x<<2));
+        System.out.println("left shift 3 of x  is    :  "+(x<<3));
+        System.out.println("left shift 4 of x  is    :  "+(x<<4));
+        System.out.println("left shift 5 of x  is    :  "+(x<<5));
+        System.out.println("left shift 6 of x  is    :  "+(x<<6));
+        System.out.println("left shift 7 of x  is    :  "+(x<<7));
+        System.out.println("left shift -1 of x  is   :  "+(-1<<1));
+	}
+
+}
---
+++ Bit Magic/Right_Shift.java
@@ -0,0 +1,16 @@
+
+public class Right_Shift {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         int x=3;
+         System.out.println("Right shift 1 of x is     :  "+(x>>1));
+         System.out.println("Right shift 2 of x is     :  "+(x>>2));
+         System.out.println("Right shift 3 of x is     :  "+(x>>3));
+         
+         // for negative number
+         int y  = -2;
+         System.out.println("Right shift 1 of y is     :  "+(x>>1));
+	}
+
+}
