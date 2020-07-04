7fb026ada73b21032ed4d1a05a1ba5977b25c004
gaurav <singhgaurav9698@gmail.com>
2020-07-04 23:28:30 +0530

    src

--- String/src/Isomorphic_Strings.java
+++ String/src/Isomorphic_Strings.java
@@ -1,21 +1,27 @@
-
+import java.util.*;
 public class Isomorphic_Strings {
-   public static boolean isomorphic(String s1,String s2)
+   public static boolean isomorphic(String S1,String S2)
    {
-	   char[] ch1 = s1.toCharArray();
-	   char[] ch2 = s2.toCharArray();
-	   for(int i = 0 ; i<s1.length();i++)
-	   {
-		   
-	   }
-	   
-	   
-	   
-	return false;   
+	   if(S1.length()!=S2.length()){
+		   return false;
+		   }
+		   int count1[] = new int[256];
+		   int count2[] = new int[256];
+		   for(int i = 0; i < S1.length(); i++){
+		   if(count1[S1.charAt(i)] != count2[S2.charAt(i)]){
+		   return false;
+		   }else{
+		   count1[S1.charAt(i)]++;
+		   count2[S2.charAt(i)]++;
+		   }
+		   }
+		   return true;
    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+       String s1 = "xyy";
+       String s2 = "abb";
+       System.out.println(isomorphic(s1,s2));
 	}
 
 }
