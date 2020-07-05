e23bf3490b86370350be4a4f7eb00d29f51d7f79
gaurav <singhgaurav9698@gmail.com>
2020-07-05 22:02:56 +0530

    src

---
+++ String/src/Keypad_Typing.java
@@ -0,0 +1,13 @@
+
+public class Keypad_Typing {
+public static void typing(String str) {
+	char[] ch = str.toCharArray();
+	
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       String str = "gaurav";
+       typing(str);
+	}
+
+}
