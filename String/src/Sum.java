06299a57187e195800b2373e61a685eab5002676
gaurav <singhgaurav9698@gmail.com>
2020-07-12 21:30:46 +0530

    src

--- String/src/Sum.java
+++ String/src/Sum.java
@@ -1,29 +1,44 @@
 
 public class Sum {
-     public static void findSum(String str)
-     {
-    	 System.out.println(str);
-    	String  s1 = str.replaceAll("[aA-zZ]", "");
-    	System.out.println(s1);
-    	//int s2 = Integer.parseInt(s1);
-    	String s[]=str.split("[a-z]+");
-    	int sum =0;
-    	for(int i=0; i<str.length()-1; i++) {
-    		if(!s[i].equals(""))
-    			sum += Integer.parseInt(s[i]);
-//    	    char temp = str.charAt(i);
-//    	    if (Character.isDigit(temp)) {
-//    	        int b = Integer.parseInt(String.valueOf(temp));
-//    	        sum=sum+b;
-    	    
-    	}
-    	System.out.println(sum);
-     }
+     static int x =10;
 	public static void main(String[] args)
      {
 		// TODO Auto-generated method stub
-       String str = "1a5v15d1d7";
-       findSum(str);
+//       int arr[] = new int[2];
+//      // System.out.println(index instanceof Object);
+////      String s1 = "UPES";
+////      String s2 = "UPES";
+////      System.out.println(s1==s2);
+//      System.out.println(arr[0]);
+		
+//		final int i;
+//		i = 70;
+//		i = 70;
+//		System.out.println(i);
+//		Integer i = 34;
+//		String str = (i<21)?"jan":(i<56)?"feb":"march";
+//		System.out.println(str);
+		
+//		 String s1="Object";  
+//		   s1=s1.concat(" Oriented");  
+//		byte i = '128';
+//		   System.out.println(i); 
+//		
+//		Integer w1 = new Integer(2);
+//		Integer w2 = new Integer(2);
+//		if(w1==w2)
+//		{
+//			System.out.println("a");
+//		}
+//		else
+//		{
+//			System.out.println("b");
+	//	}
+		
+		Sum t1 = new Sum();
+		Sum t2 = new Sum();
+		t1.x = 20;
+		System.out.print(t1.x+" "+t2.x);
 	}
 
 }
